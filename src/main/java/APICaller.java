a92a81a1d274ca94551b06a745eb21b7e1934c00
Jerry Hou <72224221+Tonyhou1999@users.noreply.github.com>
2022-12-08 01:20:04 -0500

    The finished writing and reading part of a csv file as changes have been made, it should be able to write and read csv files in the way we expected.

--- src/main/java/APICaller.java
+++ src/main/java/APICaller.java
@@ -5,18 +5,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Splitter;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.lang.reflect.Array;
+import java.net.*;
+import java.io.*;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.lang.*;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 
 public class APICaller {
@@ -32,6 +36,12 @@ public class APICaller {
         DormID.put("Gabelli",2980917);
     }
     private static HttpURLConnection conn;
+    private File all_washer_info;
+
+    private File set_info(String name) {
+        this.all_washer_info = new File(name);
+        return all_washer_info;
+    }
 
     private URL combine_URL(URL url, Map dorms, String dormname) throws MalformedURLException {
         String newURL = url.toString() + dorms.getOrDefault(dormname,"");
@@ -41,10 +51,10 @@ public class APICaller {
     /**
      *  Start creating other additional functions, 1: to make the main file shorter
      *  2: csv creation and reading csv files, i.e to convert to lists of lists and extract info from lists of lists
+     *  This is actually used to establish the connection to the scrapper website and find all the relevant information for the data, that is stored the json format
      * @param
      * @throws IOException
      */
-
     private String connection(URL url) throws IOException {
         this.conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -55,12 +65,26 @@ public class APICaller {
         return jsonData;
     }
 
+    /**
+     * This is the method that is used to actually read whatever that is presented on the website we are scraping from, and it is formatted as a String
+     * @param mapper
+     * @param jsonData
+     * @return
+     * @throws JsonProcessingException
+     */
     private List<Map<String,Object>> getValue(ObjectMapper mapper, String jsonData) throws JsonProcessingException {
         mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
         List<Map<String, Object>> data = mapper.readValue(jsonData, new TypeReference<>(){});
         return data;
     }
 
+    /**
+     * The data that contains all the information that we need, but it is not very helpful, as it is not formatted properly
+     * The goal of this function, is simply to seperate a data in a nicer way, as per machine infromation is contained in the {} block, which is
+     * what this function is doing
+     * @param data
+     * @return
+     */
     private ArrayList<String> parseData(List<Map<String,Object>> data) {
         String object = data.get(0).get("objects").toString();
         object = object.substring(1, object.length()-1);
@@ -96,6 +120,11 @@ public class APICaller {
         return dataParsed;
     }
 
+    /**
+     * This is the method that is used to get an ArrayList of HashMap by splitting the input string
+     * @param parsed
+     * @return
+     */
     private ArrayList<Map<String, String>> generateFinalData(ArrayList<String> parsed) {
         ArrayList<Map<String, String>> finalData = new ArrayList<Map<String, String>>();
         for(String str: parsed) {
@@ -120,7 +149,7 @@ public class APICaller {
         int numNotWorking = 0;
         var current = Instant.now();   //&& (map.get("appliance type:").equals(type))
         for(Map<String, String> map: finalData) {
-            if (map.get(info) != null) {
+            if (map.get(info) != null) { //Because some of the scraper code will return null, so need to check to get this waived
                 if (map.get(info).equals("Available")) {
                     currentMachine++;
                 } else if (map.get(info).equals("Out of Service")) {
@@ -136,17 +165,12 @@ public class APICaller {
                     }
                 }
             }
-            //System.out.println(map.get(info).getClass());
-            //System.out.println(map.get(info));
             current = Instant.now();
             if (currentMachine == 0) { //This means that the currentNum Washer is unavailable, so must loop through
                 current = current.plus(closetMinute, ChronoUnit.MINUTES);
             }
         }
-        //Now we have looped through the availability, and we should be able to put them into a single list
-        //The first part is to deal with the formatting of the time, we only care about the hour and the minute,
         availabilityinfo.add(formatting(current));
-
         availabilityinfo.add(dormname);
         availabilityinfo.add(String.valueOf(currentMachine));
         if(currentMachine != 0) {
@@ -158,7 +182,7 @@ public class APICaller {
     }
 
     /**
-     *  To standardize the time format, as the standrdize the time, not the actual
+     * To standardize the time format, as the standrdize the time, not the actual
      * @param  time parameter is an instant object,
      * @return The hour: minute format, in EST time zone
      */
@@ -169,9 +193,58 @@ public class APICaller {
         return ldt.getHour() + ":" + ldt.getMinute();
     }
 
+    /**
+     * We are intended to store and make updates to the csv file, but this is an initial version, then the rest will be called repetetively
+     * They key difference here is that instead of writing an array into the csv file that is being stored in the array, we are actually,
+     * trying to write every entry into the csv file, as an array that contains the dorm information from previous operations
+     * @param name, this is used for setting the name purpose
+     * @param dorm_laundry_info, this is the list that contains all the informtaion, structured in the way we wanted in our project;
+     * @throws IOException, technically should never happen
+     */
+    private void initialwriteCSV(File name, ArrayList<String> dorm_laundry_info) throws IOException {
+        CSVWriter writer = new CSVWriter(new FileWriter(name));
+        writer.writeNext(dorm_laundry_info.toArray(new String[0]));
+        writer.close();
+    }
+
+    /**
+     * We are making changes to the csv file by adding additional dorm laundry info to the csv file
+     * @param name
+     * @param dorm_laundry_info
+     * @throws IOException
+     */
+    private void updateCSV(File name, ArrayList<String> dorm_laundry_info) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader("DormInfo.csv"));
+        List<String[]> r = reader.readAll(); //Now we have read all previous data.
+        CSVWriter writer = new CSVWriter(new FileWriter(name));
+        for(String[] column : r) {
+            writer.writeNext(column);
+        }
+        writer.writeNext(dorm_laundry_info.toArray(new String[0]));
+        writer.close();
+    }
+
+    /**
+     * This method is supposed to extract the information of the csv file, which is stored as a list of lists
+     * @param dorminfo
+     * @return
+     * @throws IOException
+     */
+    private ArrayList<ArrayList<String>> getInfo(File dorminfo) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader("DormInfo.csv"));
+        List<String[]> values = reader.readAll();
+        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>;
+        for(String[] element : values) {
+            result.add((ArrayList<String>) Arrays.asList(element));
+        }
+        return null;
+    }
+
+    private void extractCSV() {
+
+    }
+
     public static void main(String[] args) throws IOException {
-        // Split two blocks of code into two functions
-        // Gets the data from the api for one laundry room, you probably want to replace that with a string
         APICaller useAPI = new APICaller() ;
         // This is the link for voute, 66 says its unavailable so I commented out the line below
         URL url = new URL("https://www.laundryview.com/api/currentRoomData?school_desc_key=12&location=");
@@ -180,34 +253,22 @@ public class APICaller {
         String jsonData = useAPI.connection(url);
         ObjectMapper mapper = new ObjectMapper();
         List<Map<String, Object>> data = useAPI.getValue(mapper,jsonData);
-
         ArrayList<String> dataParsed = useAPI.parseData(data);
-
         // Converts the arraylist to a list of hashmaps
         ArrayList<Map<String, String>> finalData =useAPI.generateFinalData(dataParsed);
         String info = "time_left_lite";
         ArrayList<String> AvailableInfo = useAPI.getAvailability(finalData,"W",info,"Gabelli");
-        //System.out.println("The following line is the list that contains the washing machine information at Gabelli Hall");
         System.out.println(AvailableInfo);
-        /*ArrayList<Map<String, String>> finalData = new ArrayList<Map<String, String>>();
-        for(String str: dataParsed) {
-            Map<String, String> hmap = Splitter.on(", ")
-                    .withKeyValueSeparator("=")
-                    .split(str);
-            finalData.add(hmap);
-        }*/
         int count = 1;
-        /* Now all you have to do is use this hashmap to determine the number of washer's available and the availability of the next washer (using a time representation but in string format)
-        if washer's are available then the availability would be zero, add this to a list of lists, and then convert that list of lists to a file, and then convert that file back to a list of lists (which I will use in my code)
-        * */
         for(Map<String, String> map: finalData) {
             System.out.println("hashmap number: " + count + " appliance type: " + map.get("appliance_type") + " availability/time left " + map.get("time_left_lite"));
             count++;
         }
-        /*System.out.println(" Now all you have to do is use this hashmap to determine the number of washer's available and the availability of the next washer (using a time representation but in string format)\n" +
-                "if washer's are available then the availability would be zero, add this to a list of lists, and then convert that list of lists to a file, \n" +
-                " and then convert that file back to a list of lists (which I will use in my code) ");
-        */
+
+        File all_washer_info = useAPI.set_info("DormInfo.csv");
+        useAPI.initialwriteCSV(all_washer_info,AvailableInfo);
+
+
 
 
 
